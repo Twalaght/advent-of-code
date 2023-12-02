94c3466958704d3ca3d01f6be06765e457f33893
Twalaght <jonathanchapman6799@gmail.com>
2023-12-02 10:40:02 +1030

    Template updates

--- template.py
+++ template.py
@@ -1,8 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 import sys
 with open(sys.argv[1] if len(sys.argv) > 1 else "input.txt") as f:
-	data = [x for x in f.read().splitlines()]
+    data = f.read().splitlines()
 
 for x in data: print(x)
 
